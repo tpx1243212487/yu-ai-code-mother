1d02f502e0cbe7b97b3a99711d6e635613937924
tpx <8257174+tanpeixing@user.noreply.gitee.com>
2026-05-16 11:11:33 +0800

    fifth part, before AI

--- src/main/java/com/yupi/yuaicodemother/service/AppService.java
+++ src/main/java/com/yupi/yuaicodemother/service/AppService.java
@@ -1,7 +1,14 @@
 package com.yupi.yuaicodemother.service;
 
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.yupi.yuaicodemother.model.dto.app.AppQueryRequest;
 import com.yupi.yuaicodemother.model.entity.App;
+import com.yupi.yuaicodemother.model.entity.User;
+import com.yupi.yuaicodemother.model.vo.AppVO;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +17,13 @@ import com.yupi.yuaicodemother.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    AppVO getAppVO(App app);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
+
+    String deployApp(Long appId, User loginUser);
 }
