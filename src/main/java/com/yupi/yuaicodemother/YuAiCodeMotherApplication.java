f6fd24c5841d1e4084b22d5115aa8e64012ac389
tpx <8257174+tanpeixing@user.noreply.gitee.com>
2026-05-17 19:49:47 +0800

    sixth part 3

--- src/main/java/com/yupi/yuaicodemother/YuAiCodeMotherApplication.java
+++ src/main/java/com/yupi/yuaicodemother/YuAiCodeMotherApplication.java
@@ -1,11 +1,12 @@
 package com.yupi.yuaicodemother;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @MapperScan("com.yupi.yuaicodemother.mapper")
+@SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 public class YuAiCodeMotherApplication {
 
     public static void main(String[] args) {
