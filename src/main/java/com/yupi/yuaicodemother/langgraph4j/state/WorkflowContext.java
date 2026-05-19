236e257c5aaffca8d9bcc2bb2e2e3bff07a135fe
tpx <8257174+tanpeixing@user.noreply.gitee.com>
2026-05-19 10:42:57 +0800

    tenth part 3, 并发和子图 使用langgraph4j parallel branch 实现并发

--- src/main/java/com/yupi/yuaicodemother/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/yupi/yuaicodemother/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.yupi.yuaicodemother.langgraph4j.state;
 
+import com.yupi.yuaicodemother.langgraph4j.model.ImageCollectionPlan;
 import com.yupi.yuaicodemother.langgraph4j.model.ImageResource;
 import com.yupi.yuaicodemother.langgraph4j.model.QualityResult;
 import com.yupi.yuaicodemother.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,20 @@ public class WorkflowContext implements Serializable {
      * 质量检查结果
      */
     private QualityResult qualityResult;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
 
     @Serial
     private static final long serialVersionUID = 1L;
